59f290ea4d3f27584af9bc722adb45fe4b04256b
Litti8 <lisandropiatti@gmail.com>
2025-05-14 13:39:15 +0200

    feat: Define models, add migrations, configure DB connection

--- fake_commerce_api/settings.py
+++ fake_commerce_api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'products', 
 ]
 
 MIDDLEWARE = [
@@ -74,8 +80,12 @@ WSGI_APPLICATION = 'fake_commerce_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'fake_commerce_db'), 
+        'USER': os.environ.get('DB_USER', 'user'),             
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'), 
+        'HOST': os.environ.get('DB_HOST', 'localhost'),        
+        'PORT': os.environ.get('DB_PORT', '5432'),            
     }
 }
 
--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Categories" # Corrige el nombre en el admin
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(max_length=500, blank=True, null=True) 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    sizes = models.CharField(max_length=255, help_text="Comma separated sizes, e.g., S,M,L,XL")
+    
+
+    def __str__(self):
+        return self.title
+
